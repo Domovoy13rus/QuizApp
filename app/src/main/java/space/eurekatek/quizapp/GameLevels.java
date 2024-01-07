19443dc4e10f889163308a3817d94c73ad08be2d
Domovoy13rus <zheka2001@list.ru>
2024-01-07 15:29:30 +0300

    Add Level1.java, universal.xml

--- app/src/main/java/space/eurekatek/quizapp/GameLevels.java
+++ app/src/main/java/space/eurekatek/quizapp/GameLevels.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -36,7 +37,22 @@ public class GameLevels extends AppCompatActivity {
             }
         });
 
+        // Кнопка перехода на первый уровень - начало
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    startActivity(intent); finish();
+                }catch (Exception e){
+                    // пусто
+                }
+            }
+        });
+        // Кнопка перехода на первый уровень - конец
     }
+
     // Системная кнопка "Назад" - начало
     @Override
     public void onBackPressed(){
